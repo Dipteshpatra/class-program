cc8bda370ebf243bea507dbeef38c6d1a7e4e083
Dipteshpatra <dipteshp@moback.com>
2022-09-30 10:15:21 +0530

    Add project files.

---
+++ car program/Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_program
+{
+    class car
+    {
+        private int id;
+        private string carname;
+        private string colour;
+        private int maxspeed;
+        public int carid
+        {
+            get { return id; }
+            set { id = value; } 
+        }
+
+        public string CarNAME
+        {
+            get { return carname; }
+            set { carname = value; }    
+        }
+        public string Colour
+        {
+            get { return colour; }
+            set { colour = value; } 
+        }
+        public int Maxspeed
+        {
+            get { return maxspeed; }
+            set { maxspeed = value; }
+        }
+        public void type()
+        {
+            if (id < 200)
+                Console.WriteLine("type of the car is Manual and petrol");
+            else
+                Console.WriteLine("type of the car is automatic and disel");
+            
+        }
+        public void warning()
+        {
+            if (maxspeed > 100)
+                Console.WriteLine("Please think about your family");
+            else
+             Console.WriteLine("you are in right track sir");
+        }
+
+    }
+}
---
+++ car program/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_program
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            car car1 = new car();
+            car1.carid = 201;
+            car1.CarNAME = "ford";
+            car1.Colour = "Black";
+            car1.Maxspeed = 150;
+            car1.type();
+            car1.warning();
+            Console.WriteLine("Car id\t" + car1.carid);
+            Console.WriteLine("Car name is\t"+car1.CarNAME);
+            Console.WriteLine("Car colour is\t"+car1.Colour);
+            Console.WriteLine("Car maxspeed\t"+car1.Maxspeed);
+
+            Console.WriteLine("Second car detalis");
+
+            car car2 = new car();
+            car2.carid = 124;
+            car2.CarNAME = "Maruti";
+            car2.Colour= "White";
+            car2.Maxspeed = 84;
+            Console.WriteLine("Car id\t" + car2.carid);
+            Console.WriteLine("Car id\t" + car2.CarNAME);
+            Console.WriteLine("Car colour is\t" + car2.Colour);
+            Console.WriteLine("Car maxspeed\t"+car2.Maxspeed);
+            car2.type();
+            car2.warning(); 
+
+        }
+    }
+}
